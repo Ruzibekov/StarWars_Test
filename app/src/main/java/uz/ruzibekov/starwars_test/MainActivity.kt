6634f70766d4437dcfbc5f109cfa79129fe649e2
ruzibekov <ruzibekov44@gmail.com>
2023-07-25 12:46:57 +0500

    add: dagger-hilt

--- app/src/main/java/uz/ruzibekov/starwars_test/MainActivity.kt
+++ app/src/main/java/uz/ruzibekov/starwars_test/MainActivity.kt
@@ -3,25 +3,24 @@ package uz.ruzibekov.starwars_test
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.activity.viewModels
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import uz.ruzibekov.starwars_test.ui.navigation.MainNavHost
+import dagger.hilt.android.AndroidEntryPoint
+import uz.ruzibekov.starwars_test.ui.MainViewModel
 import uz.ruzibekov.starwars_test.ui.theme.StarWars_TestTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             val navController = rememberNavController()
 
             StarWars_TestTheme {
---
+++ app/src/main/java/uz/ruzibekov/starwars_test/ui/app/StarWarsApp.kt
@@ -0,0 +1,8 @@
+package uz.ruzibekov.starwars_test.ui.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StarWarsApp : Application() {
+}
